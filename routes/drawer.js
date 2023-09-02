64eecccfae170e52fdd7292c4607f5caa827c5bb
munib dalati <munib.dalati@gmail.com>
2023-09-02 10:43:45 +0300

    finish all 35 videos

--- routes/drawer.js
+++ routes/drawer.js
@@ -5,14 +5,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import About from "../screens/about";
 import Home from "../screens/home";
 
-
 const Drawer = createDrawerNavigator();
 
 export default function myDrawer() {
+  let heartLogo = require('../assets/heart_logo.png')
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeStack} />
+        <Drawer.Screen
+          name= "GameZone"
+          component={HomeStack}
+          options={{ drawerLabel: "Home" }}
+        />
         <Drawer.Screen name="About" component={About} />
       </Drawer.Navigator>
     </NavigationContainer>
--- screens/home.js
+++ screens/home.js
@@ -1,16 +1,57 @@
 import React, { useState } from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { globalStyles } from "../styles/global";
-import Card from "../shared/card"
+import Card from "../shared/card";
+import { MaterialIcons } from "@expo/vector-icons";
+import ReviewForm from "./reviewForm";
 
 export default function Home({ navigation }) {
+  const [modalOpen, setModalOpen] = useState(false);
   const [reviews, setReviews] = useState([
     { title: "Title 1", rating: 5, body: "Body 1", key: "1" },
     { title: "Title 2", rating: 4, body: "Body 2", key: "2" },
     { title: "Title 3", rating: 3, body: "Body 3", key: "3" },
   ]);
+
+  const addReview = (review) => {
+    review.key = Math.random().toString();
+    setReviews((currentReviews) => {
+      return [review, ...currentReviews];
+    });
+    // to hide the modal after submission
+    setModalOpen(false);
+  };
   return (
     <View style={globalStyles.container}>
+      {/* modal */}
+      <Modal visible={modalOpen} animationType="slide">
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={StyleSheet.modalContent}>
+          <MaterialIcons
+            name="close"
+            size={24}
+            style={{ ...styles.modalToggle, ...styles.modalClose }}
+            onPress={() => setModalOpen(false)}
+          />
+          <ReviewForm addReview={addReview} />
+        </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+      <MaterialIcons
+        name="add"
+        size={24}
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)}
+      />
       <FlatList
         data={reviews}
         renderItem={({ item }) => (
@@ -26,3 +67,21 @@ export default function Home({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#f2f2f2",
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: "center",
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+  },
+  modalContent: {
+    flex: 1,
+  },
+});
